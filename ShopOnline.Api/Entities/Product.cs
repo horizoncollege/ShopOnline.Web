0570ddb73b41b885ac1e0ce49678b159a159ba2e
ferdi330 <70571055+ferdi330@users.noreply.github.com>
2023-02-06 14:27:37 +0100

    Migrations / Database / Appsettings.json / Dto.s aangemaakt.

--- ShopOnline.Api/Entities/Product.cs
+++ ShopOnline.Api/Entities/Product.cs
@@ -6,16 +6,16 @@ namespace ShopOnline.Api.Entities
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public string ImageURL { get; set; }
+        public string? ImageURL { get; set; }
 
         public decimal Price { get; set; }
 
         public int Qty { get; set; }
 
-        public int Category { get; set; }
+        public int CategoryId { get; set; }
     }
 }
