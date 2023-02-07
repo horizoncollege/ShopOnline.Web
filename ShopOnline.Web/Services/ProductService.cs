74ffcdba9f5634e868f32e87e413e2a630bad238
ferdi330 <70571055+ferdi330@users.noreply.github.com>
2023-02-07 12:21:18 +0100

    Services/Contracts aangemaakt in ShopOnline.Web

---
+++ ShopOnline.Web/Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public Task<IEnumerable<ProductDto>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
