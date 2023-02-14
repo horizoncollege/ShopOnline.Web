b7d10e3ad7dee3075b98aad421a2489a4a1d7b80
ferdi330 <70571055+ferdi330@users.noreply.github.com>
2023-02-14 10:50:38 +0100

    Shoppingcart

---
+++ ShopOnline.Api/Repositories/Contracts/ShoppingCartRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public class ShoppingCartRepository : IShoppingCartRepository
+    {
+        private readonly ShopOnlineDbContext shopOnlineDbContext;
+
+        public ShoppingCartRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.shopOnlineDbContext = shopOnlineDbContext;
+        }
+
+        private async Task<bool> CartItemExists(int cartId, int productId)
+        {
+            return await this.shopOnlineDbContext.CartItems.AnyAsync(c => c.CartId == cartId &&
+                                                                     c.ProductId == productId);
+        }
+
+        public async Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto)
+        {
+            var item = await (from product in this.shopOnlineDbContext.Products
+                              where product.Id == cartItemToAddDto.ProductId
+                              select new CartItem
+                              {
+                                  CartId = cartItemToAddDto.CartId,
+                                  ProductId = product.Id,
+                                  Qty = cartItemToAddDto.Qty
+                              }).SingleOrDefaultAsync();
+
+            if (item != null)
+            {
+                var result = await this.shopOnlineDbContext.CartItems.AddAsync(item);
+                await this.shopOnlineDbContext.SaveChangesAsync();
+                return result.Entity;
+            }
+
+            return null;
+        }
+
+        public Task<CartItem> DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CartItem>> GetItems(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
